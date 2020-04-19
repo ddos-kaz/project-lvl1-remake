468d5d66352b97e41a7ca241386718650b84f89f
Damir <damir.doszhan@gmail.com>
2020-04-19 21:12:45 +0600

    Remake of 1st Hexlet project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import readlineSync from 'readline-sync';
+
+const getUserName = () => {
+    console.log('Welcome to the Brain Games!\n');
+
+    const userName = readlineSync.question('May I have your name? ');
+
+    console.log(`Hello, ${userName}`);
+};
+
+export default getUserName;
